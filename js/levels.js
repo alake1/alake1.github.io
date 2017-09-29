0c72d3faccb3c539f68f9ffbd772e7deeba744da
Utk <utkala@gmail.com>
2017-09-29 08:45:37 +0530

    ad

--- js/levels.js
+++ js/levels.js
@@ -1,10 +1,10 @@
 function randomnum(slideIndex) {
     if (slideIndex==1) {
-    var notRandomNumbers = [2, 2, 2, 2, 2, 4, 8, 8, 8, 8];
+    var notRandomNumbers = [2, 2, 2, 2, 4, 4, 8, 8, 8, 8];
     } else if (slideIndex==2) {
-        var notRandomNumbers = [2, 2, 2, 2, 4, 4, 4, 8, 8, 8, 8];
+        var notRandomNumbers = [2, 2, 2, 4, 4, 4, 8, 8, 8, 8];
     } else {
-        var notRandomNumbers = [2, 2, 4, 4, 8, 8];
+        var notRandomNumbers = [2, 4, 4, 4, 8, 8];
     }
     var idx = Math.floor(Math.random() * notRandomNumbers.length);
     return notRandomNumbers[idx];
